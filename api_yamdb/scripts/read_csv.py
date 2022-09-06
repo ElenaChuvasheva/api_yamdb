289b927b32655d1aad223dda2b20e8f32e68d1a6
Елена Чувашева <chuvashevalena@yandex.ru>
2022-09-06 17:33:51 +0300

    добавлена роль юзера, код подготовлен под расширение

--- api_yamdb/scripts/read_csv.py
+++ api_yamdb/scripts/read_csv.py
@@ -2,18 +2,27 @@ import csv
 import os
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from users.models import CustomUser
 
 CSV_DIR = os.path.join(settings.BASE_DIR, 'static', 'data')
 
-filename = 'users.csv'
-filepath = os.path.join(CSV_DIR, filename)
 
-with open(filepath, 'r') as f:
-    reader = list(csv.reader(f))
-    reader.pop(0)
-    for row in reader:
-        print(f'{row[0]} {row[1]} {row[2]}')
-        User.objects.create(pk=int(row[0]), username=row[1], email=row[2])
+def read_to_DB(filename, DBclass):
+    filepath = os.path.join(CSV_DIR, filename)
+    with open(filepath, 'r') as f:
+        reader = list(csv.reader(f))
+        reader.pop(0)
+        for row in reader:
+            print(f'{row[0]} {row[1]} {row[2]} {row[3]}')
+            howto_create(DBclass, row)
+
+
+def howto_create(DBclass, row):
+    if DBclass == CustomUser:
+        kwargs = {'pk': int(row[0]), 'username': row[1], 'email': row[2],
+                  'role': row[3]}
+    DBclass.objects.create(**kwargs)
+
+
+read_to_DB('users.csv', CustomUser)
